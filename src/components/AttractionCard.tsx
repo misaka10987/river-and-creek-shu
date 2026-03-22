101f074eafbf43f7eb5b8f0fc1d24f4f2f55b6f7
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-03-22 17:14:35 +0800

    use shadcn card

--- src/components/AttractionCard.tsx
+++ src/components/AttractionCard.tsx
@@ -1,27 +1,45 @@
-import ReactMarkdown from "react-markdown";
-import type { Attraction } from "@/lib/attractions";
+import ReactMarkdown from 'react-markdown'
+import type { Attraction } from '@/lib/attractions'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Button } from './ui/button'
 
 interface Props {
-  attraction: Attraction;
-  onClose: () => void;
+  attraction: Attraction
+  onClose: () => void
 }
 
 export default function AttractionCard({ attraction, onClose }: Props) {
   return (
-    <div className="flex flex-col bg-white dark:bg-zinc-900 rounded-lg shadow-lg border border-zinc-200 dark:border-zinc-800 p-4 max-h-full">
-      <div className="flex justify-between items-center mb-2">
-        <div className="font-bold text-lg truncate">{attraction.name}</div>
-        <button
-          onClick={onClose}
-          className="text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200 text-2xl leading-none"
-          aria-label="关闭"
-        >
-          ×
-        </button>
-      </div>
-      <div className="prose prose-sm flex-1 overflow-y-auto text-xs dark:prose-invert">
-        <ReactMarkdown>{attraction.content}</ReactMarkdown>
-      </div>
-    </div>
-  );
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>{attraction.name}</CardTitle>
+          <CardDescription>{attraction.coordinate.join(', ')}</CardDescription>
+          <CardAction>
+            <Button
+              variant={'ghost'}
+              onClick={onClose}
+              className="text-2xl leading-none"
+              aria-label="关闭"
+            >
+              ×
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <ReactMarkdown>{attraction.content}</ReactMarkdown>
+        </CardContent>
+        {/* <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter> */}
+      </Card>
+    </>
+  )
 }
