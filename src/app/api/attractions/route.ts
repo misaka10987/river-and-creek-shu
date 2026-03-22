dc2330eca462909bd73cff5f3f4ca88bf8e4a993
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-03-22 15:10:59 +0800

    use edge runtime

--- src/app/api/attractions/route.ts
+++ src/app/api/attractions/route.ts
@@ -2,6 +2,8 @@
 import { NextResponse } from "next/server";
 import { getAttractions } from "@/lib/attractions";
 
+export const runtime = 'edge';
+
 export async function GET() {
   return NextResponse.json(getAttractions());
 }
